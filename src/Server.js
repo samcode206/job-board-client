1fe62f4c5f54fe8a7570c3cd60fd585396c9ad31
samcode206 <66322745+samcode206@users.noreply.github.com>
2021-05-17 23:27:20 -0700

    Update Server.js

--- src/Server.js
+++ src/Server.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const baseURL = axios.create({
-    baseURL: "https://job-board-services.herokuapp.com"
+    baseURL: "http://ec2-35-164-223-25.us-west-2.compute.amazonaws.com:5000"
 });
 
 
